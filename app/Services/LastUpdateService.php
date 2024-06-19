3eaf2caa20ea10ceb8e8ae40fbfc2a9fc39ff343
Tudor Rusu <tudorr89@gmail.com>
2024-06-19 17:50:14 +0200

    simplify last update service

--- app/Services/LastUpdateService.php
+++ app/Services/LastUpdateService.php
@@ -2,23 +2,14 @@
 
 namespace App\Services;
 
-use App\Models\Company;
 use Carbon\Carbon;
 use App\Settings\GeneralSettings;
 
 class LastUpdateService
 {
-    public static function getDate()
-    {
-        return Carbon::parse(app(GeneralSettings::class)->lastUpdate);
-    }
-
     public static function needsUpdate()
     {
-        $lastUpdate = self::getDate();
-        $now = Carbon::now();
-
-        return $lastUpdate->diffInMonths($now) >= 4;
+        return Carbon::parse(app(GeneralSettings::class)->lastUpdate)->diffInMonths(Carbon::now()) >= 4;
     }
 
     public static function setDate()
