2e6df7910b0d172230689a4dcbb0a28a845db7b6
tudorr89 <tudorr89@gmail.com>
2026-01-04 10:29:13 +0200

    Fix: JSON-LD formatting cu unescaped slashes și unicode

--- resources/views/components/seo-meta.blade.php
+++ resources/views/components/seo-meta.blade.php
@@ -93,4 +93,6 @@
         ];
     }
 @endphp
-<script type="application/ld+json">{{ json_encode($schema) }}</script>
+<script type="application/ld+json">
+{!! json_encode($schema, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) !!}
+</script>
