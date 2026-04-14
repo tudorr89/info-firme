870a31ca4af34c1c60548490c9c351358d96d9aa
tudorr89 <tudorr89@gmail.com>
2026-04-14 18:07:45 +0300

    disable UI

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,18 @@ use App\Http\Controllers\CompanyController;
 use App\Livewire\CompanySearch;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', CompanySearch::class)->name('company.search');
-Route::get('/companie/{cui}', [CompanyController::class, 'show'])
-    ->name('company.show')
-    ->where('cui', '[0-9]+');
+//Route::get('/', CompanySearch::class)->name('company.search');
+//Route::get('/companie/{cui}', [CompanyController::class, 'show'])
+//    ->name('company.show')
+//    ->where('cui', '[0-9]+');
 
 Route::fallback(function () {
     abort(401, 'Unauthorized');
 });
 
 // Sitemap routes - use API middleware to avoid session cookies
-Route::withoutMiddleware(['web'])->group(function () {
-    Route::get('/sitemap.xml', [SitemapController::class, 'index']);
-    Route::get('/api/sitemap-{number}.xml', [SitemapController::class, 'chunk'])
-        ->where('number', '[0-9]+');
-});
+//Route::withoutMiddleware(['web'])->group(function () {
+//    Route::get('/sitemap.xml', [SitemapController::class, 'index']);
+//    Route::get('/api/sitemap-{number}.xml', [SitemapController::class, 'chunk'])
+//        ->where('number', '[0-9]+');
+//});
